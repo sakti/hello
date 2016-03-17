09637a55b7b2dc5507e610e4e7c2a0884d5143a1
Sakti Dwi Cahyono <54krpl@gmail.com>
2016-03-17 13:06:38 +0700

    add more go files

---
+++ cmd/world.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/sakti/hello/hello"
+)
+
+func main() {
+	fmt.Println(world)
+	fmt.Println(hello.Version)
+}
---
+++ hello/lib.go
@@ -0,0 +1,3 @@
+package hello
+
+var Version = 1.0
